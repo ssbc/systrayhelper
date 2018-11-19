267bf66db850b021bc8f8ba67d3665d6cc0abfb6
Henry <cryptix@riseup.net>
2018-11-19 13:47:05 +0100

    tune nixpkgs override

--- override.nix
+++ override.nix
@@ -2,5 +2,9 @@ with import <unstable> { };
 systrayhelper.overrideDerivation (drv: { 
   name = "systrayhelper-fromsrc";
   src = ./.;
+  buildFlagsArray = [ ''-ldflags=
+    -X main.version=snap
+    -X main.date="nix-byrev"
+  '' ];
 })
 
